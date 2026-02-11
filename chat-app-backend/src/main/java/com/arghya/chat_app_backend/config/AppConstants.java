8736af9f92a7dd788c96a55cd5a6eca980a6e0a3
Arghya Chakraborty <arghya0812@gmail.com>
2026-02-12 02:29:55 +0530

    frontend url

--- chat-app-backend/src/main/java/com/arghya/chat_app_backend/config/AppConstants.java
+++ chat-app-backend/src/main/java/com/arghya/chat_app_backend/config/AppConstants.java
@@ -1,5 +1,5 @@
 package com.arghya.chat_app_backend.config;
 
 public class AppConstants {
-    public static final String FRONT_END_BASE_URL = "http://localhost:5173";
+    public static final String FRONT_END_BASE_URL = "https://chatapplatest.vercel.app";
 }
