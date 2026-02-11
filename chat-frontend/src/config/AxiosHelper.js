fd30e5f1eed4a1cbf285ce5683701ec36c14de8a
Arghya Chakraborty <arghya0812@gmail.com>
2026-02-12 02:24:19 +0530

    render url

--- chat-frontend/src/config/AxiosHelper.js
+++ chat-frontend/src/config/AxiosHelper.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-export const baseUrl = "http://localhost:8080";
+export const baseUrl = "https://chatapp-latest-bkvj.onrender.com";
 const httpClient = axios.create({
     baseURL: baseUrl,
 });
