411f862af1ff61b22701983daec68fba97984a1e
Arghya Chakraborty <arghya0812@gmail.com>
2026-03-04 20:27:12 +0530

    new url

--- chat-app-backend/src/main/java/com/arghya/chat_app_backend/config/AppConstants.java
+++ chat-app-backend/src/main/java/com/arghya/chat_app_backend/config/AppConstants.java
@@ -1,6 +1,6 @@
 package com.arghya.chat_app_backend.config;
 
 public class AppConstants {
-    public static final String FRONT_END_BASE_URL = "https://chat-app-jade-three-84.vercel.app";
+    public static final String FRONT_END_BASE_URL = "https://chat-dyxhqc8wf-arghya-chakrabortys-projects.vercel.app";
 }
 // production : https://chatapplatest.vercel.app
