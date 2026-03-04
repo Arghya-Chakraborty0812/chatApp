c0f1d06944ffa30af1b235b6980473eb79911a31
Arghya Chakraborty <arghya0812@gmail.com>
2026-03-04 20:38:39 +0530

    fixes

--- chat-app-backend/src/main/java/com/arghya/chat_app_backend/controllers/UserController.java
+++ chat-app-backend/src/main/java/com/arghya/chat_app_backend/controllers/UserController.java
@@ -7,12 +7,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.arghya.chat_app_backend.config.AppConstants;
 import com.arghya.chat_app_backend.entities.User;
 import com.arghya.chat_app_backend.repositories.UserRepository;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin("*")
+@CrossOrigin(origins = AppConstants.FRONT_END_BASE_URL)
 public class UserController {
 
     @Autowired
